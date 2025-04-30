85e4574489aa1fda7ca4c21595e36e5e4e8dd994
daniilsjb <daniilb@outlook.com>
2025-04-30 14:28:02 +0300

    Refer to datacubes as hypercubes

--- suspectral-notebook/scripts/hsi2envi.py
+++ suspectral-notebook/scripts/hsi2envi.py
@@ -13,33 +13,33 @@ parent = Path('.').parent
 
 def extract_icvl(name: str):
     with h5py.File(parent / 'datasets' / 'ICVL' / f'{name}.mat', 'r') as file:
-        datacube = np.array(file['rad']).swapaxes(0, 2)
+        hypercube = np.array(file['rad']).swapaxes(0, 2)
         wavelengths = np.squeeze(file['bands'])
 
-    return datacube, wavelengths
+    return hypercube, wavelengths
 
 
 def extract_cave(name: str):
     with os.scandir(parent / 'datasets' / 'CAVE' / name) as files:
         bands = list(iio.imread(f.path) for f in files if f.name.endswith('.png'))
 
-    datacube = np.dstack(bands) / np.iinfo(np.uint16).max
+    hypercube = np.dstack(bands) / np.iinfo(np.uint16).max
     wavelengths = np.arange(400, 700 + 1, 10)
-    return datacube, wavelengths
+    return hypercube, wavelengths
 
 
 def extract_kaust(name: str):
     with h5py.File(parent / 'datasets' / 'KAUST' / f'{name}.h5', 'r') as file:
-        datacube = np.array(file['img\\']).swapaxes(0, 2)
+        hypercube = np.array(file['img\\']).swapaxes(0, 2)
         wavelengths = np.arange(400, 730 + 1, 10)
 
-    return datacube, wavelengths
+    return hypercube, wavelengths
 
 
 def extract_harvard(name: str):
-    datacube = sio.loadmat(str(parent / 'datasets' / 'Harvard' / f'{name}.mat'))['ref']
+    hypercube = sio.loadmat(str(parent / 'datasets' / 'Harvard' / f'{name}.mat'))['ref']
     wavelengths = np.arange(420, 720 + 1, 10)
-    return datacube, wavelengths
+    return hypercube, wavelengths
 
 
 def main():
@@ -66,13 +66,13 @@ def main():
     args = parser.parse_args()
     match args.dataset:
         case 'icvl':
-            datacube, wavelengths = extract_icvl(args.name)
+            hypercube, wavelengths = extract_icvl(args.name)
         case 'cave':
-            datacube, wavelengths = extract_cave(args.name)
+            hypercube, wavelengths = extract_cave(args.name)
         case 'kaust':
-            datacube, wavelengths = extract_kaust(args.name)
+            hypercube, wavelengths = extract_kaust(args.name)
         case 'harvard':
-            datacube, wavelengths = extract_harvard(args.name)
+            hypercube, wavelengths = extract_harvard(args.name)
         case _:
             raise Exception(f'Unknown dataset: {args.dataset}')
 
@@ -83,7 +83,7 @@ def main():
     }
 
     hdr = Path(args.destination) / f'{args.name}.hdr'
-    envi.save_image(hdr, datacube, ext='raw', metadata=metadata, force=True)
+    envi.save_image(hdr, hypercube, ext='raw', metadata=metadata, force=True)
 
 
 if __name__ == '__main__':
